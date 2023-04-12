6477d1496ad4456d3298787b0d7ad954418ae772
huangdongjiang <dongjiang.huang@get88.cn>
2023-04-12 17:15:56 +0800

    feat: 生成render函数

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# mini-vue2
+# vue1.0
+
+- 数据响应式拦截
+  - 普通对象
+  - 数组
+- 数据响应式更新
+  - 依赖收集
+    - Dep
+    - Watcher
+  - 编译器
+    - 文件节点
+    - v-on:click
+    - v-bind
+    - v-model
---
+++ src/vue2.0/utils.js
@@ -0,0 +1,27 @@
+/**
+ * 将 key 代理到 target 上，
+ * 比如 代理 this._data.xx 为 this.xx
+ * @param {*} target 目标对象，比如 vm
+ * @param {*} sourceKey 原始 key，比如 _data
+ * @param {*} key 代理的原始对象上的指定属性，比如 _data.xx
+ */
+export function proxy(target, sourceKey, key) {
+  Object.defineProperty(target, key, {
+    // target.key 的读取操作实际上返回的是 target.sourceKey.key
+    get() {
+      return target[sourceKey][key];
+    },
+    // target.key 的赋值操作实际上是 target.sourceKey.key = newV
+    set(newV) {
+      target[sourceKey][key] = newV;
+    },
+  });
+}
+
+/**
+ * 是否为自闭合标签，内置一些自闭合标签，为了处理简单
+ */
+export function isUnaryTag(tagName) {
+  const unaryTag = ['input'];
+  return unaryTag.includes(tagName);
+}
